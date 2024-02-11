0cf045b873201b7ee2e3d25faaf3e9ba2c3c291f
sanchitapatel29 <sanchitapatel29@gmail.com>
2024-02-11 19:48:52 +0530

    half completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent,
+    },
+    {
+        path:'about',
+        component: AboutComponent,
+    },
+    // {
+    //     path:'footer',
+    //     component: FooterComponent,
+    // },
+];
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
